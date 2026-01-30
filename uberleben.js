cb799861668a477747c67fa21f5d591f4d65323b
Michael Kaszynski <mkaszynski2000@gmail.com>
2026-01-30 13:49:54 -0700

    version 1.4.6 another attempt to fix chest bug

--- uberleben.js
+++ uberleben.js
@@ -548,6 +548,8 @@ function loop() {
     }
     render1 = true;
 
+    if (chests.length == 0) chests = {};
+
     block_posx = Math.floor(posx/SIZE);
     block_posy = Math.floor(posy/SIZE);
 
@@ -1223,7 +1225,7 @@ function loop() {
     
     ctx.fillStyle = "white";          // text color
     ctx.font = "12px Arial";          // font size and family
-    ctx.fillText("Version 1.4.5", 20, 50);
+    ctx.fillText("Version 1.4.6", 20, 50);
 
     
     if (550 < mouse.x && mouse.x < 650 && 350 < mouse.y && mouse.y < 450 && mouse.held[0]) {
